ea10e8ad71f173d856f63f161ab3cae62d684bbd
Simon Berner <sibebzh@gmail.com>
2021-02-08 22:06:13 +0100

    feat: :sparkles: add the dicee magic roll algo

--- dicee-ios/ViewController.swift
+++ dicee-ios/ViewController.swift
@@ -1,19 +1,21 @@
-//
-//  ViewController.swift
-//  dicee-ios
-//
-//  Created by Simon Berner on 08.02.21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    let leftDiceNumber = Int.random(in:0...5)
+    let rightDiceNumber = Int.random(in:0...5)
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")].randomElement()
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")].randomElement()
+    }
+    
 }
 
